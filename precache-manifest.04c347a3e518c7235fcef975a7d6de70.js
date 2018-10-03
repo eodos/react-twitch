c4c0ead57623accf9730a5069eb25e9e57dfc32e
David Paul Pena <eodos0@gmail.com>
2018-10-02 22:41:16 -0700

    Updates

---
+++ precache-manifest.04c347a3e518c7235fcef975a7d6de70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eac23d999143f26842d",
+    "url": "/react-twitch/static/js/runtime~main.1eac23d9.js"
+  },
+  {
+    "revision": "cbce32638262e3bf5649",
+    "url": "/react-twitch/static/js/main.cbce3263.chunk.js"
+  },
+  {
+    "revision": "9093b22ed18fcc015e5c",
+    "url": "/react-twitch/static/js/1.9093b22e.chunk.js"
+  },
+  {
+    "revision": "cbce32638262e3bf5649",
+    "url": "/react-twitch/static/css/main.1eeaeedb.chunk.css"
+  },
+  {
+    "revision": "9093b22ed18fcc015e5c",
+    "url": "/react-twitch/static/css/1.3653b804.chunk.css"
+  },
+  {
+    "revision": "af44c45573a5366d4acaab2cf034308f",
+    "url": "/react-twitch/index.html"
+  }
+];
